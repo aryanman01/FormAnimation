a0194f41efbab7cc290dfe46e8c0b43621ecc8c7
Ankit Aryan <aryanman097@gmail.com>
2020-11-04 00:44:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+function animatedForm(){
+    const arrows = document.querySelectorAll(".fa-arrow-down");
+
+    arrows.forEach(arrow => {
+        arrow.addEventListener("click", ()=> {
+            const input = arrow.previousElementSibling;
+            const parent = arrow.parentElement;
+            const nextForm = parent.nextElementSibling;
+
+            if(input.type === "text" && validateUser(input)) {
+                nextSlide(parent,nextForm);
+            }
+        });
+    });
+}
+
+function validateUser(user){
+    if (user.value.length < 6) {
+        console.log("not enough characters");
+        error("rgb(189,87,87)");
+    }
+    else {
+        error("rgb(87,189,130)");
+        return true;
+    }
+}
+
+function nextSlide(parent, nextForm){
+    parent.classList.add("inactive");
+    parent.classList.remove("active");
+    nextForm.classList.add("active");
+}
+
+function error(color){
+    document.body.style.backgroundColor = color;
+}
+
+
+animatedForm();
